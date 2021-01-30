d597e1b51b7673176083d99b462d5736ccde4d15
Hayk <hayk_safaryan@epam.com>
2021-01-30 23:36:29 +0400

    implement networking with weather api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+
+import '../secrets.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,18 +10,25 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
+  double lat;
+  double lon;
+
+  void getLocationData() async {
     print('clicked');
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latitude);
+    lat = location.latitude;
+    lon = location.longitude;
+    final url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
   @override
