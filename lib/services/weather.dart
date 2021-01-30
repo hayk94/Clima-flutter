b4f5619394771dfa75e79907f4573a806d36a0a7
Hayk <hayk_safaryan@epam.com>
2021-01-31 01:00:31 +0400

    implement city weather functionality

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -2,12 +2,21 @@ import 'package:clima/secrets.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
     final url =
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric';
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric';
     NetworkHelper networkHelper = NetworkHelper(url);
     return await networkHelper.getData();
   }
